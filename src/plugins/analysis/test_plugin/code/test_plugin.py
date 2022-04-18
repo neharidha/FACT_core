196e632704902d58e9c4e4643c888fbadb1212a7
BOerAck <102927384+BOerAck@users.noreply.github.com>
2022-04-17 20:05:21 -0400

    added dns resolver

--- src/plugins/analysis/test_plugin/code/test_plugin.py
+++ src/plugins/analysis/test_plugin/code/test_plugin.py
@@ -1,4 +1,5 @@
 from ipaddress import ip_address
+from dns import resolver
 
 from analysis.PluginBase import AnalysisBasePlugin
 from helperFunctions.compare_sets import substring_is_in_list
@@ -18,11 +19,54 @@ class AnalysisPlugin(AnalysisBasePlugin):
 		super().__init__(plugin_administrator, config=config, recursive=recursive, timeout=timeout, plugin_path=__file__)
 
 	def process_object(self, file_object):
+		final_data = {} #dict of original artifact mapped to analysis
 		result = self.ip_and_uri_finder.analyze_file(file_object.file_path, separate_ipv6=True)
 		for key in ['uris', 'ips_v4', 'ips_v6']:
 		    result[key] = self._remove_duplicates(result[key])
-		file_object.processed_analysis[self.NAME] = self._get_augmented_result(result)
+		for key, data_list in result.items():
+			if key not in ['uris', 'ips_v4', 'ips_v6']:
+				continue
+			for data in data_list:
+				print(f'$ {data} - {type(data)}')
+				if type(data) == list:
+					print(f'##DATA LIST: {data}')
+					data = data[0]
+				if key == 'uris':
+					data = self.get_domains_from_uri(data)
+					final_data[data] = self.get_ips_from_domain(data)
+				elif key == 'ips_v4':
+					final_data[data] = self.get_domains_from_ip(data)
+		print(f'##################\n\n{final_data}\n\n################')
+		file_object.processed_analysis[self.NAME] = final_data #self._get_augmented_result(result)
 		return file_object
+		
+	def get_domains_from_uri(self, uri):
+		sub_strs = uri.split("://")
+		if len(sub_strs) == 1: #case where no protocol
+			domain = (sub_strs[0])
+		else:
+			domain = (sub_strs[1].split("/")[0])
+		return domain
+		
+
+	def get_ips_from_domain(self, domain):
+
+		ips = []
+		print(f'requesting {domain}')
+		response = resolver.resolve(domain,'A')
+		for ip in response:
+			ips.append(str(ip))
+		return ips
+				
+	def get_domains_from_ip(self, ip):
+		domains = []
+		print(f'requesting {ip}')
+		response = resolver.resolve_address(ip)
+		for domain in response:
+			domains.append(str(domain))
+
+		return domains
+		
 		
 	def _get_augmented_result(self, result):
 		result['summary'] = self._get_summary(result)
@@ -42,5 +86,7 @@ class AnalysisPlugin(AnalysisBasePlugin):
 			for i in results[key]:
 				summary.append(i[0])
 		return summary
+
+
 
 
